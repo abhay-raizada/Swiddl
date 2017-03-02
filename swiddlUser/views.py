8badd45f8b8326ef943c2342670b47b5e98437eb
Abhay Raizada <Abhay Raizada>
2017-03-02 22:39:47 +0530

    Add View files

--- swiddlUser/views.py
+++ swiddlUser/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.conf import settings
+from django.shortcuts import render, redirect, Http404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the users index.")
+    context = {}
+    return render(request, 'swiddlUser/index.html', context)
 # Create your views here.
